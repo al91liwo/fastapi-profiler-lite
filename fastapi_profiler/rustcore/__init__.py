ae0e83f20959be0fb6b4b379bbfb5549877163f6
Alexander Lind <al91liwo@gmail.com>
2025-04-04 12:48:36 +0200

    ci: Fix build pipeline, release fastapi-profiler-rust only on main

--- fastapi_profiler/rustcore/__init__.py
+++ fastapi_profiler/rustcore/__init__.py
@@ -1,4 +1,4 @@
-from .rustcore import PyAggregatedStats
+from rustcore import PyAggregatedStats
 
 __version__ = "0.2.6"
 __all__ = ["PyAggregatedStats", "__version__"]
--- fastapi_profiler/stats.py
+++ fastapi_profiler/stats.py
@@ -1,7 +1,7 @@
 import json
 from typing import Dict, List
 
-from rustcore.fastapi_profiler_rust import PyAggregatedStats
+from rustcore import PyAggregatedStats
 
 
 class AggregatedStats:
